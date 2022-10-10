2238e911da01a1c66e2fd40a1bec5f1b36aa349e
Julien Muggli <liozon@hotmail.com>
2022-10-10 15:28:56 +0200

    Update Keep session open.ps1

--- Keep session open.ps1
+++ Keep session open.ps1
@@ -1,13 +1,16 @@
 Clear-Host
-Write-Output "Keep-alive with Scroll Lock..."
+Write-Output "Keep-alive with Scroll lock..."
 Write-Output " "
 Write-Output "Keep this windows open and the script will work in the background !"
 
 $WShell = New-Object -com "Wscript.Shell"
+$sleep = 30
 
 while ($true) {
   $WShell.sendkeys("{SCROLLLOCK}")
   Start-Sleep -Milliseconds 100
+  Write-Host "Pressed Scroll lock"
   $WShell.sendkeys("{SCROLLLOCK}")
-  Start-Sleep -Seconds 60
+  Write-Host "Waiting "$sleep" seconds" 
+  Start-Sleep -Seconds $sleep
 }
